4e60788f90f6baaae6e309c0d38867b4de1b1d94
bShaak <bradenshaak@gmail.com>
2015-07-12 00:00:45 -0700

    login users and test valid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		#log in
+      log_in user
+      redirect_to user
   	else
   		flash.now[:danger] = 'Invalid emai/password combination'
   		render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+	#log in the given user
+	#uses session method provided by rails which creates a temp cookie in browser
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	#return the current logged in user
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	#check if a user is logged into the site
+	def logged_in?
+		!current_user.nil?
+	end
 end
