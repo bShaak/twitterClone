8161aed2fdc00bb23d7de578a16399d5eb7805cc
bShaak <bradenshaak@gmail.com>
2015-08-16 23:31:08 -0700

    add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,11 +6,11 @@ class UserTest < ActiveSupport::TestCase
   						password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should be valid" do 
+  test "should be valid" do
   	assert @user.valid?
   end
 
-  test "name should be present" do 
+  test "name should be present" do
   	@user.name = "       "
   	assert_not @user.valid?
   end
@@ -57,5 +57,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated should return false for a user with a nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
