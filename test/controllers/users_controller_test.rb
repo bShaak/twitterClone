e4147ce99f5868db5595323fec1a46439c74a208
bShaak <bradenshaak@gmail.com>
2015-08-22 22:49:09 -0700

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -42,14 +42,14 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
-  test "should redirect destroy when not logged in" do 
-    assert_no_difference 'User.count' do 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
     assert_redirected_to login_url
   end
 
-  test "should redirect to destroy when logged in as a non-admin" do 
+  test "should redirect to destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
@@ -57,12 +57,22 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test "should not allow the admin attribue to be edited via the web" do 
+  test "should not allow the admin attribue to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch :update, id: @other_user, user: {password: "foobar",
                                             password_confirmation: "foobar",
                                             admin: true}
-    assert_not @other_user.admin?      
-  end                                      
+    assert_not @other_user.admin?
+  end
+
+	test "should redirect following when not logged in" do
+		get :following, id: @user
+		assert_redirected_to login_url
+	end
+
+	test "should redirect followers when not logged in" do
+		get :followers, id: @user
+		assert_redirected_to login_url
+	end
 end
